346a5e99e5a0d4fe95e094c5ae336fcc5ec2a087
tooqk4u <tooqk4u@protonmail.com>
2022-02-12 18:45:07 -0500

    added html routes

--- routes/apiRoutes/noteRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { notes } = require('../../db/db.json');
-
-router.get('/notes', (req, res) => {
-    res.send('Hello!')
-});
-
-module.exports = router; 
