27a90c14b7a3ec69f94d82644f6bf6a7818a7728
tooqk4u <tooqk4u@protonmail.com>
2022-02-12 18:52:57 -0500

    added api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,38 @@
+// require dependencies
+const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
-const noteRoutes = require('./noteRoutes');
+const { notes } = require('../../db/db.json');
+const uniqid = require('uniqid');
 
-router.use(noteRoutes);
 
-module.exports = router; 
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+
+router.post('/notes', (req, res) => {
+    
+    req.body.id = uniqid();
+
+    
+    if (!req.body.title || !req.body.text) {
+        res.status(400).send('Please enter title and text.');
+    } else {
+        const note = req.body;
+
+        notes.push(note);
+
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            JSON.stringify({ notes: notes }, null, 2)
+        );
+
+        res.json(notes);
+    }
+});
+
+
+
+
+module.exports = router;
