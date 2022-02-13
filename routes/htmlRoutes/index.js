3351b72c7c5525c4e855b0680fbe7448d4ba8d6a
tooqk4u <tooqk4u@protonmail.com>
2022-02-12 19:11:38 -0500

    added comments to htmlRoutes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,20 +1,20 @@
 const path = require('path');
 const router = require('express').Router();
 
-
+// get request for index.html page
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-
+// get request for note.html page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
-
+// get request for * 
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-
+// export router
 module.exports = router;
