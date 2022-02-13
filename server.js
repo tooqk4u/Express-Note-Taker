35ab1a637b59b463c5991222d5718d9ee061e286
tooqk4u <tooqk4u@protonmail.com>
2022-02-12 19:19:34 -0500

    added comments to server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
@@ -13,5 +12,4 @@ app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-}); 
-
+});
